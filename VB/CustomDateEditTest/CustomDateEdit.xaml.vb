608e8997fbc272fd004e59d82749b17678270261
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomDateEditTest/CustomDateEdit.xaml.vb
@@ -0,0 +1,44 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows
+Imports DevExpress.Xpf.Editors
+Imports DevExpress.Xpf.Utils
+
+Namespace CustomDateEditTest
+	Partial Public Class CustomDateEdit
+		Inherits DateEdit
+
+		#Region "Special Dates property"
+		Public Shared ReadOnly SpecialDatesProperty As DependencyProperty
+		Public Shared ReadOnly SpecialBorderStyleProperty As DependencyProperty
+
+		Public Property SpecialDates() As IList(Of DateTime)
+			Get
+				Return CType(GetValue(SpecialDatesProperty), IList(Of DateTime))
+			End Get
+			Set(ByVal value As IList(Of DateTime))
+				SetValue(SpecialDatesProperty, value)
+			End Set
+		End Property
+		Public Property SpecialBorderStyle() As Style
+			Get
+				Return CType(GetValue(SpecialBorderStyleProperty), Style)
+			End Get
+			Set(ByVal value As Style)
+				SetValue(SpecialBorderStyleProperty, value)
+			End Set
+		End Property
+		#End Region
+		Public Sub New()
+			InitializeComponent()
+			SpecialBorderStyle = TryCast(FindResource("defaultSpecialBorderStyle"), Style)
+		End Sub
+
+		Shared Sub New()
+			SpecialDatesProperty = DependencyPropertyManager.Register("SpecialDates", GetType(IList(Of DateTime)), GetType(CustomDateEdit))
+			SpecialBorderStyleProperty = DependencyPropertyManager.Register("SpecialBorderStyle", GetType(Style), GetType(CustomDateEdit))
+		End Sub
+	End Class
+End Namespace
---
+++ VB/CustomDateEditTest/SpecialDateBorderStyleConverter.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Markup
+Imports System.Windows.Data
+Imports DevExpress.Xpf.Editors.Popups.Calendar
+Imports System.Windows
+
+Namespace CustomDateEditTest
+	Public Class SpecialDateBorderStyleConverter
+		Inherits MarkupExtension
+		Implements IMultiValueConverter
+
+		Public Function Convert(ByVal values() As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IMultiValueConverter.Convert
+			Dim specialDatesList As List(Of DateTime) = TryCast(values(0), List(Of DateTime))
+			Dim currentCellButton As CalendarCellButton = CType(values(1), CalendarCellButton)
+			Dim dt As DateTime = CDate(DateEditCalendar.GetDateTime(CType(currentCellButton, DependencyObject)))
+dt=dt.Date
+			Return specialDatesList.Contains(dt)
+		End Function
+
+		Public Function ConvertBack(ByVal value As Object, ByVal targetTypes() As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object() Implements IMultiValueConverter.ConvertBack
+			Throw New NotImplementedException()
+		End Function
+
+		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+			Return Me
+		End Function
+	End Class
+End Namespace
