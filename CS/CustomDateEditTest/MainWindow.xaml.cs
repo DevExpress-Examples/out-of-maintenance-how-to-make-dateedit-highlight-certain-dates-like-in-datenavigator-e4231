5bc9f6b73dd8e60fcd83a637003080846fc3d45b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomDateEditTest/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace CustomDateEditTest {
+    public partial class MainWindow : Window {
+        List<DateTime> dates;
+
+        public List<DateTime> Dates {
+            get {
+                return dates;
+            }
+            set {
+                dates = value;
+            }
+        }
+
+        public MainWindow() {
+            InitializeComponent();
+            FillDates();
+        }
+
+        public void FillDates() {
+            dates = new List<DateTime>();
+            dates.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 3));
+            dates.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 4));
+            dates.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 5));
+            dates.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 12));
+            dates.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 13));
+            dates.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 14));
+            dates.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 21));
+            dates.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 22));
+            dates.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month, 23));
+        }
+    }
+}
---
+++ VB/CustomDateEditTest/SpecialDateBorderStyleConverter.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Markup
+Imports System.Windows.Data
+Imports DevExpress.Xpf.Editors.Popups.Calendar
+Imports System.Windows
+
+Namespace CustomDateEditTest
+    Public Class SpecialDateBorderStyleConverter
+        Inherits MarkupExtension
+        Implements IMultiValueConverter
+
+        Public Function Convert(ByVal values() As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IMultiValueConverter.Convert
+            Dim specialDatesList As List(Of Date) = TryCast(values(0), List(Of Date))
+            Dim currentCellButton As CalendarCellButton = DirectCast(values(1), CalendarCellButton)
+            Dim dt As Date = CDate(DateEditCalendar.GetDateTime(CType(currentCellButton, DependencyObject)))
+dt=dt.Date
+            Return specialDatesList.Contains(dt)
+        End Function
+
+        Public Function ConvertBack(ByVal value As Object, ByVal targetTypes() As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object() Implements IMultiValueConverter.ConvertBack
+            Throw New NotImplementedException()
+        End Function
+
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+            Return Me
+        End Function
+    End Class
+End Namespace
