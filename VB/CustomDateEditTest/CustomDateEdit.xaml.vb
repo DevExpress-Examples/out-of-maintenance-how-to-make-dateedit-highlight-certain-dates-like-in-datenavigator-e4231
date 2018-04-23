f3d94937aaa544c99384c850f21483be7cb02a04
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomDateEditTest/CustomDateEdit.xaml.vb
@@ -0,0 +1,42 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows
+Imports DevExpress.Xpf.Editors
+Imports DevExpress.Xpf.Utils
+
+Namespace CustomDateEditTest
+    Partial Public Class CustomDateEdit
+        Inherits DateEdit
+
+        #Region "Special Dates property"
+        Public Shared ReadOnly SpecialDatesProperty As DependencyProperty
+        Public Shared ReadOnly SpecialBorderStyleProperty As DependencyProperty
+
+        Public Property SpecialDates() As IList(Of Date)
+            Get
+                Return DirectCast(GetValue(SpecialDatesProperty), IList(Of Date))
+            End Get
+            Set(ByVal value As IList(Of Date))
+                SetValue(SpecialDatesProperty, value)
+            End Set
+        End Property
+        Public Property SpecialBorderStyle() As Style
+            Get
+                Return CType(GetValue(SpecialBorderStyleProperty), Style)
+            End Get
+            Set(ByVal value As Style)
+                SetValue(SpecialBorderStyleProperty, value)
+            End Set
+        End Property
+        #End Region
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Shared Sub New()
+            SpecialDatesProperty = DependencyPropertyManager.Register("SpecialDates", GetType(IList(Of Date)), GetType(CustomDateEdit))
+            SpecialBorderStyleProperty = DependencyPropertyManager.Register("SpecialBorderStyle", GetType(Style), GetType(CustomDateEdit))
+        End Sub
+    End Class
+End Namespace
